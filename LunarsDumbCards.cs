384e8de3b54e1af6b378eddfd89f5fa16472a40a
LunarHackett003 <LunarHackett003@users.noreply.github.com>
2023-10-29 10:00:28 +0000

    Tiny changes

--- LunarsDumbCards.cs
+++ LunarsDumbCards.cs
@@ -15,14 +15,12 @@ namespace LunarsCards
         internal static AssetBundle assets;
         void Awake()
         {
-            UnityEngine.Debug.Log("Wake the fuck up, Unity!");
             assets = Jotunn.Utils.AssetUtils.LoadAssetBundleFromResources("lunarscards", typeof(LunarsDumbCards).Assembly);
-            UnityEngine.Debug.Log("Woah, we loaded this thing");
             assets.LoadAsset<GameObject>("ModCards").GetComponent<CardHolder>().RegisterCards();
         }
         void Start()
         {
-            UnityEngine.Debug.Log("Wake the fuck up, Unity!");
+            UnityEngine.Debug.Log("You're using Lunar's Dumb Cards! I am now changing your gender.");
         }
     }
 }
