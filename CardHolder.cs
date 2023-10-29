5a2bb099849e8eb06680b030ba8e95ce40d889dc
LunarHackett003 <LunarHackett003@users.noreply.github.com>
2023-10-29 09:47:59 +0000

    First Push

---
+++ CardHolder.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections.Generic;
+using System.Runtime.InteropServices.ComTypes;
+using UnboundLib.Cards;
+using UnityEngine;
+
+namespace LunarsCards
+{
+    public class CardHolder : MonoBehaviour
+    {
+        public List<GameObject> Cards;
+        public List<GameObject> HiddenCards;
+
+        internal void RegisterCards()
+        {
+            foreach (var Card in Cards)
+            {
+                CustomCard.RegisterUnityCard(Card, LunarsDumbCards.modInitials, Card.GetComponent<CardInfo>().cardName, true, null);
+            }
+            foreach (var Card in HiddenCards)
+            {
+                CustomCard.RegisterUnityCard(Card, LunarsDumbCards.modInitials, Card.GetComponent<CardInfo>().cardName, false, null);
+                ModdingUtils.Utils.Cards.instance.AddHiddenCard(Card.GetComponent<CardInfo>());
+            }
+        }
+    }
+}
---
+++ LunarsDumbCards.cs
@@ -0,0 +1,28 @@
+﻿using BepInEx;
+using System.Runtime.InteropServices.ComTypes;
+using UnityEngine;
+namespace LunarsCards
+{
+    [BepInDependency("com.willis.rounds.unbound")]
+    [BepInDependency("pykess.rounds.plugins.moddingutils")]
+    [BepInDependency("pykess.rounds.plugins.cardchoicespawnuniquecardpatch")]
+    [BepInPlugin("lunar.rounds.dumbcards", "Lunars Dumb Cards", "0.0.1")]
+    [BepInProcess("Rounds.exe")]
+
+    public class LunarsDumbCards : BaseUnityPlugin
+    {
+        internal static string modInitials = "LDC";
+        internal static AssetBundle assets;
+        void Awake()
+        {
+            UnityEngine.Debug.Log("Wake the fuck up, Unity!");
+            assets = Jotunn.Utils.AssetUtils.LoadAssetBundleFromResources("lunarscards", typeof(LunarsDumbCards).Assembly);
+            UnityEngine.Debug.Log("Woah, we loaded this thing");
+            assets.LoadAsset<GameObject>("ModCards").GetComponent<CardHolder>().RegisterCards();
+        }
+        void Start()
+        {
+            UnityEngine.Debug.Log("Wake the fuck up, Unity!");
+        }
+    }
+}
